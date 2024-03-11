5a3f40fc03c3beaf269ee86f9af5d8a8bd63219c
Shohboz Xurramov <shohboztuit@gmail.com>
2024-03-11 18:03:19 +0500

    INFRA:Initialize Unit Test Project

---
+++ Sheenam.Api.Tests.Unit/DeleteMe.cs
@@ -0,0 +1,16 @@
+﻿//==================================================
+//Copyright (c) Coalition of Good-Hearted Enginners
+//Free To Use Comfort and Pleace
+//==================================================
+namespace Sheenam.Api.Tests.Unit
+{
+    public class DeleteMe
+    {
+        [Fact]
+        public void ShouldBeTrue() =>
+            Assert.True(true);
+           
+        
+
+    }
+}
